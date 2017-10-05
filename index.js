cdbbb466604dabc6c53fd636b2ee29be52fa3256
Theodore Bugnet <theodorebugnet@kcl.ac.uk>
2017-10-05 09:31:38 +0100

    stop propagation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //*
 $(function() {
     $('.selectModuleButton').on('click', function(e) {
+        e.stopPropagation();
         var moduleId = e.target.getAttribute('module');
         $('div.module#' + moduleId).toggleClass('selectedModule');
     });
