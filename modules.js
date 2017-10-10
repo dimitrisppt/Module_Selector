3ceadf12e34e09f325ff753a2bd0adef366d5c8e
Aakash <k1630608@kcl.ac.uk>
2017-10-10 10:46:55 +0100

    added optional modules

--- modules.js
+++ modules.js
@@ -6,10 +6,10 @@ window.moduleData = {
     core: {
         moduleList: [
             {
-                id: "6CCSCOMP",
-                title: "6CCSCOMP Compulsory Module",
+                id: "6CCS3PRJ",
+                title: "Individual Project",
                 credits: 30,
-                term: "1st term",
+                term: "1 and 2",
                 selected: true
             }
         ],
@@ -18,16 +18,130 @@ window.moduleData = {
     optional: {
         moduleList: [
             {
-                id: "6CCSOPT",
-                title: "Optional module",
+                id: "6CCS3AMS",
+                title: "6CCS3AMS - Agents and Multi-Agent Systems",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3CFL",
+                title: "6CCS3CFL - Compiler and Formal Languages",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3COV",
+                title: "6CCS3COV - Computer Vision",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3CSL",
+                title: "6CCS3CSL - Computer Science Logic",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3HCI",
+                title: "6CCS3HCI - Human-Computer Interaction",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3VER",
+                title: "6CCS3VER - Formal Verification",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3WSN",
+                title: "6CCS3WSN - Algorithms for the World Wide Web and Social Networks",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3SMT",
+                title: "6CCS3SMT - Software Measurement and Testing",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3GRS",
+                title: "6CCS3GRS - Computer Graphics Systems",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3PAL",
+                title: "6CCS3PAL - Parallel Algorithms",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3AIN",
+                title: "6CCS3AIN - Artificial Intelligence",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3CIS",
+                title: "6CCS3CIS - Cryptography and Information Security",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "6CCS3AIP",
+                title: "6CCS3AIP - Artificial Intelligence Planning",
+                credits: 15,
+                term: "1st term"
+            },
+            {
+                id: "",
+                title: "6CCS3DSM - Distributed Systems",
                 credits: 15,
                 term: "2nd term"
             },
             {
-                id: "6CCSAI",
-                title: "6CCSAI Some Sort of AI Module",
+                id: "6CCS3COM",
+                title: "6CCS3COM - Computational Models",
                 credits: 15,
-                term: "1st term"
+                term: "2nd term"
+            },
+            {
+                id: "6CCS3SAD",
+                title: "6CCS3SAD - Software Architecture and Design",
+                credits: 15,
+                term: "2nd term"
+            },
+            {
+                id: "6CCS3SIA",
+                title: "6CCS3SIA - Software Engineering of Internet Applications",
+                credits: 15,
+                term: "2nd term"
+            },
+            {
+                id: "6CCS3NSE",
+                title: "6CCS3NSE - Network Security",
+                credits: 15,
+                term: "2nd term"
+            },
+            {
+                id: "6CCS3OME",
+                title: "6CCS3OME - Optimisation Methods",
+                credits: 15,
+                term: "2nd term"
+            },
+            {
+                id: "6CCS3PRE",
+                title: "6CCS3PRE - Pattern Recognition",
+                credits: 15,
+                term: "2nd term"
+            },
+            {
+                id: "6CCS3TSP",
+                title: "6CCS3TSP - Text Searching and Processing",
+                credits: 15,
+                term: "2nd term"
             }
         ],
         isCore: false
