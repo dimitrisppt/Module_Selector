b97290c44fce38186d0d23025ef9d2c42cdd0957
Manuel Martinez <manuel@Manuels-MacBook-Pro.local>
2017-10-14 18:31:01 +0100

    Flexibox for layout and made selection lists scrollable

--- modules.js
+++ modules.js
@@ -11,12 +11,50 @@ window.moduleData = {
                 credits: 30,
                 term: "1st term",
                 selected: true
-            }
+            },
+            {
+                id: "6CCSCOMP",
+                title: "6CCSCOMP Compulsory Module",
+                credits: 30,
+                term: "1st term",
+                selected: true
+            },
+            {
+                id: "6CCSCOMP",
+                title: "6CCSCOMP Compulsory Module",
+                credits: 30,
+                term: "1st term",
+                selected: true
+            },
         ],
         isCore: true
     },
     optional: {
         moduleList: [
+            {
+                id: "6CCSOPT",
+                title: "Optional module",
+                credits: 15,
+                term: "2nd term"
+            },
+            {
+                id: "6CCSOPT",
+                title: "Optional module",
+                credits: 15,
+                term: "2nd term"
+            },
+            {
+                id: "6CCSOPT",
+                title: "Optional module",
+                credits: 15,
+                term: "2nd term"
+            },
+            {
+                id: "6CCSOPT",
+                title: "Optional module",
+                credits: 15,
+                term: "2nd term"
+            },
             {
                 id: "6CCSOPT",
                 title: "Optional module",
