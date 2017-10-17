dec4e0a9fb7c5e3f0dd5ca00b809c2936461bbab
Aakash <k1630608@kcl.ac.uk>
2017-10-17 10:59:09 +0100

    displays module information after clicking the module

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ var template = '{{#moduleList}}\
 </div>\
 {{/moduleList}}';
 
+
+
+
 $(function() {
     $('#coreModuleList').append(Mustache.render(template, moduleData.core));
     $('#optionalModuleListTerm1').html(Mustache.render(template, moduleData.term1));
@@ -20,11 +23,40 @@ $(function() {
         var moduleId = e.target.getAttribute('module');
         $('div.module#' + moduleId).toggleClass('selectedModule');
     });
-
-    $('.module').on('click', function(e)){
+    //*
+    $('.module').on('click', function(e){
         e.stopPropagation();
-        var informationText = //get text here
-        $('InfoPanelLink').hide();
-        $('#InfoPanelInformation').text(informationText);
-    }
+        var moduleId = e.currentTarget.getAttribute('id');
+        var moduleArray;
+        
+        if (e.currentTarget.parentElement.getAttribute('id') === 'coreModuleList'){
+            moduleArray = window.moduleData.core.moduleList;
+        }else if(e.currentTarget.parentElement.getAttribute('id') === 'optionalModuleListTerm1'){
+            moduleArray = window.moduleData.term1.moduleList;
+        }else if(e.currentTarget.parentElement.getAttribute('id') === 'optionalModuleListTerm2'){
+            moduleArray = window.moduleData.term2.moduleList;
+        }
+        
+
+        for(module of moduleArray){
+            if(moduleId === module.id){
+                var title = module.title
+                var credits = module.credits;
+                var lecturer = module.lecturer;
+                var assessment = module.assessment;
+                var desc = module.description;
+
+                $('#InfoPanelLink').hide();
+                $('#InfoPanelInformation').hide();//*/
+                $('#moduleId').html('<b>Module ID: </b>' + moduleId + "<br>");
+                $('#moduleTitle').html('<b>Module Title: </b>' + title + "<br>");
+                $('#moduleCredits').html('<b>Module Credits: </b>' + credits + "<br>");
+                $('#moduleLecturer').html('<b>Module Lecturer(s): </b>' + lecturer + "<br>");
+                $('#moduleAssessment').html('<b>Module Marking: </b>' + assessment + "<br>");
+                $('#moduleDescription').html('<b>Module Description: </b>' + desc + "<br>");//*/
+            }
+        }
+
+
+    });
 });
